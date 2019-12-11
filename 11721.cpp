44f16fb5b602f8f688c6fca54408c3df5ef26092
dbwlsals <dbwlsals@naver.com>
2019-12-11 22:59:01 +0900

    Add files via upload

---
+++ 11721.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+
+    for(int i=0; i<str.size(); i++)
+    {
+        cout << str[i];
+        if((i+1)%10==0)
+        {
+            cout << '\n';
+        }
+    }
+}
