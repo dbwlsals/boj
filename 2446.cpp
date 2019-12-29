701ec0c8ab72505878ffc9f0809fd8706eca3015
dbwlsals <dbwlsals@naver.com>
2019-12-29 23:53:44 +0900

    Add files via upload

---
+++ 2446.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout << " ";
+        }
+        for(int k=0; k<2*(n-i)-1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int i=2; i<=n; i++)
+    {
+        for(int j=1; j<=n-i; j++)
+        {
+            cout << " ";
+        }
+        for(int k=0; k<=2*(i-1); k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
